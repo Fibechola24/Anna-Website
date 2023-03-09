2a93a9f532843151ac5037244951a4d3dba4f2ae
Victor Okpanachi <okpanachivictor0@gmail.com>
2023-03-09 13:39:00 +0100

    wire up contact form

--- js/app.js
+++ js/app.js
@@ -1,18 +1,47 @@
-(function (app){
-    app.homepage = function(){
-        setCopyrightDate();
+(function (app) {
+    app.homepage = function () {
+      setCopyrightDate();
+      wireContactForm();
     };
-
-    app.portfolio = function(){
-        setCopyrightDate();
+  
+    app.portfolio = function () {
+      setCopyrightDate();
     };
-
-    app.workItem = function(){
-        setCopyrightDate();
+  
+    app.workItem = function () {
+      setCopyrightDate();
     };
-
-    function setCopyrightDate(){
-        const date = new Date();
-        document.getElementById('copyrightYear').innerText = date.getFullYear();
+  
+    function setCopyrightDate() {
+      const date = new Date();
+      document.getElementById('copyrightYear').innerText = date.getFullYear();
     }
-})((window.app = window.app || {}));
+  
+    function wireContactForm() {
+      const contactForm = document.getElementById('contact-form');
+      contactForm.onsubmit = contactFormSubmit;
+    }
+  
+    function contactFormSubmit(e) {
+      e.preventDefault();
+  
+      const contactForm = document.getElementById('contact-form');
+      const name = contactForm.querySelector('#name');
+      const email = contactForm.querySelector('#email');
+      const message = contactForm.querySelector('#message');
+
+      if (!name || !email || !message) {
+        console.error('Could not find input elements');
+        return;
+      }
+    
+  
+      const mailTo = `mailto:${email.value}?subject=ContactFrom ${name.value}&body=${message.value}`;
+      window.open(mailTo);  //open a link i.e an email client. ? means parameters
+  
+      name.value = '';
+      email.value = '';
+      message.value = '';   //clear up contact after sending
+    }
+  })((window.app = window.app || {}));
+  
