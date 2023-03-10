20cf5de38ee2bbbf2c88c76102ac819ab855ce03
Victor Okpanachi <okpanachivictor0@gmail.com>
2023-03-11 00:04:16 +0100

    dynamic page update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 (function (app) {
+    app.portfolioItems = [];
+    app.selectedItem = {};
+
     app.homepage = function () {
       setCopyrightDate();
       wireContactForm();
@@ -10,6 +13,9 @@
   
     app.workItem = function () {
       setCopyrightDate();
+      loadPageData();
+      loadSpecificItem(); 
+      updateItemPage();
     };
   
     function setCopyrightDate() {
@@ -43,5 +49,64 @@
       email.value = '';
       message.value = '';   //clear up contact after sending
     }
+
+    async function loadPageData() {
+        const cacheData = sessionStorage.getItem('site-data');
+    
+        if (cacheData !== null) {
+          app.portfolioItems = JSON.parse(cacheData);
+        } else {
+          const rawData = await fetch('sitedata.json');
+          const data = await rawData.json();
+          app.portfolioItems = data;
+          sessionStorage.setItem('site-data', JSON.stringify(data));
+        }
+      }
+
+      function loadSpecificItem() {
+        const params = new URLSearchParams(window.location.search);
+        let item = Number.parseInt(params.get('item'));
+    
+        if (item > app.portfolioItems.length || item < 1) {
+          item = 1;
+        }
+    
+        app.selectedItem = app.portfolioItems[item - 1];
+        app.selectedItem.id = item;
+      }
+
+      
+ //update the web page
+ function updateItemPage() {
+    const header = document.getElementById('work-item-header');
+    header.innerText = `0${app.selectedItem.id}. ${app.selectedItem.title}`;
+
+    const image = document.getElementById('work-item-image');
+    image.src = app.selectedItem.largeImage;
+    image.alt = app.selectedItem.largeImageAlt;
+
+    const projectText = document.querySelector('#project-text p');
+    projectText.innerText = app.selectedItem.projectText;
+
+    const originalTechList = document.querySelector('#technologies-text ul');
+    const technologySection = document.getElementById('technologies-text');
+    const ul = document.createElement('ul');
+
+
+    app.selectedItem.technologies.forEach(el => {
+        const li = document.createElement('li');
+        li.innerText = el;
+        ul.appendChild(li);
+      });
+
+      originalTechList.remove();
+      technologySection.appendChild(ul);
+
+      const challengesText = document.querySelector('#challenges-text p');
+      challengesText.innerText = app.selectedItem.challengesText;
+
+ }
+
+
   })((window.app = window.app || {}));
   
